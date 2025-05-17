4d2e758656fc3ce18d449b7d148db1b85af48d46
deepanshubajaj <deepanshu.bajaj98@gmail.com>
2025-05-17 12:54:52 +0530

    Implemented add function for comma-separated numbers and basic test cases

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,5 +1,11 @@
 export function add(numbers: string): number {
-    
-    return 0; 
+    // If the string is empty
+    if (numbers === "") return 0;
+  
+    // Splitting the string by commas, and converting each part to a number, and sum them
+    return numbers
+      .split(",")               
+      .map((num) => parseInt(num, 10))  
+      .reduce((sum, num) => sum + num, 0);  
   }
   
